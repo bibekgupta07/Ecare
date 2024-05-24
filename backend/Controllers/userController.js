2922ca5fb53434284923382f9740de90a01ec0d5
bibekgupta07 <phenomenalbibek@gmail.com>
2024-05-24 09:30:15 +0545

    users can update their profile now

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,4 +1,6 @@
 import User from "../models/UserSchema.js";
+import Booking from "../models/BookingSchema.js";
+import Doctor from "../models/DoctorSchema.js";
 
 export const updateUser = async (req, res) => {
   const id = req.params.id;
@@ -16,9 +18,11 @@ export const updateUser = async (req, res) => {
       data: updateUser,
     });
   } catch (err) {
-    res
-      .status(500)
-      .json({ success: false, message: "Failed to  updated", data: updateUser });
+    res.status(500).json({
+      success: false,
+      message: "Failed to  updated",
+      data: updateUser,
+    });
   }
 };
 
@@ -58,7 +62,6 @@ export const getAllUser = async (req, res) => {
 
   try {
     const users = await User.find({}).select("-password");
-   
 
     res.status(200).json({
       success: true,
@@ -66,7 +69,59 @@ export const getAllUser = async (req, res) => {
       data: users,
     });
   } catch (err) {
-    
     res.status(404).json({ success: false, message: "Not found" });
+  }
+};
+
+export const getUserProfile = async (req, res) => {
+  const userId = req.userId;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not Found" });
+    }
+
+    const { password, ...rest } = user._doc;
+
+    res.status(200).json({
+      success: true,
+      message: "Profile info is getting",
+      data: { ...rest },
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "something we wrong, cannot get" });
+  }
+};
+
+export const getMyAppointments = async (req, res) => {
+  try {
+    //step1 : retrive appointments from booking
+    const bookings = await Booking.find({ user: req.userId });
+
+    //step2: extract doctor ids from the appointment booking
+    const doctorIds = bookings.map((el) => el.doctor.id);
+
+    //step3: retrieve doctors using doctor ids
+    const doctors = await Doctor.find({ _id: { $in: doctorIds } }).select(
+      "-password"
+    );
+
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "Appointments are getting",
+        data: doctors,
+      });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "Something went wrong, cannot get" });
   }
 };
